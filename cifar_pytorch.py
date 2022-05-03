3e4292c79d6d8e155722d9e885153c70642c057e
bikram <bikcrum@gmail.com>
2022-05-02 21:48:40 -0700

    auto format

--- cifar_pytorch.py
+++ cifar_pytorch.py
@@ -3,12 +3,12 @@ import itertools
 import random
 import logging
 import pickle
+
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 import numpy as np
-
 
 # Imports for Pytorch for the things we need
 import torch
@@ -24,14 +24,13 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        #TODO
+        # TODO
 
     def forward(self, x):
-        #TODO
+        # TODO
         return x
 
     def num_flat_features(self, x):
@@ -42,62 +41,58 @@ class Net(nn.Module):
         return num_features
 
 
-
 class CIFAR3(Dataset):
 
-    def __init__(self,split="train",transform=None):
-      if split=="train":
-        with open("cifar10_hst_train", 'rb') as fo:
-          self.data = pickle.load(fo) 
-      elif split=="val":
-        with open("cifar10_hst_val", 'rb') as fo:
-          self.data = pickle.load(fo)
-      else:
-        with open("cifar10_hst_test", 'rb') as fo:
-          self.data = pickle.load(fo)
-      
-      self.transform = transform
+    def __init__(self, split="train", transform=None):
+        if split == "train":
+            with open("cifar10_hst_train", 'rb') as fo:
+                self.data = pickle.load(fo)
+        elif split == "val":
+            with open("cifar10_hst_val", 'rb') as fo:
+                self.data = pickle.load(fo)
+        else:
+            with open("cifar10_hst_test", 'rb') as fo:
+                self.data = pickle.load(fo)
+
+        self.transform = transform
 
     def __len__(self):
         return len(self.data['labels'])
 
     def __getitem__(self, idx):
-        
-        x = self.data['images'][idx,:]
-        r = x[:1024].reshape(32,32)
-        g = x[1024:2048].reshape(32,32)
-        b = x[2048:].reshape(32,32)
-        
-        x = Tensor(np.stack([r,g,b]))
+
+        x = self.data['images'][idx, :]
+        r = x[:1024].reshape(32, 32)
+        g = x[1024:2048].reshape(32, 32)
+        b = x[2048:].reshape(32, 32)
+
+        x = Tensor(np.stack([r, g, b]))
 
         if self.transform is not None:
-          x = self.transform(x)
-        
-        y = self.data['labels'][idx,0]
-        return x,y 
+            x = self.transform(x)
+
+        y = self.data['labels'][idx, 0]
+        return x, y
+
+    #########################################################
 
 
-
-
-
-
-#########################################################
 # Training and Evaluation
 #########################################################
 
 
 train_transform = transforms.Compose([
-        transforms.ColorJitter(),
-        transforms.RandomRotation(30),
-        transforms.RandomHorizontalFlip(),
-        transforms.Normalize(mean=[127.5, 127.5, 127.5],
-                             std=[127.5, 127.5, 127.5])
-    ])
+    transforms.ColorJitter(),
+    transforms.RandomRotation(30),
+    transforms.RandomHorizontalFlip(),
+    transforms.Normalize(mean=[127.5, 127.5, 127.5],
+                         std=[127.5, 127.5, 127.5])
+])
 
 test_transform = transforms.Compose([
-        transforms.Normalize(mean=[127.5, 127.5, 127.5],
-                             std=[127.5, 127.5, 127.5])
-    ])
+    transforms.Normalize(mean=[127.5, 127.5, 127.5],
+                         std=[127.5, 127.5, 127.5])
+])
 
 train_data = CIFAR3("train", transform=train_transform)
 val_data = CIFAR3("val", transform=test_transform)
@@ -108,10 +103,8 @@ trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_wo
 valloader = DataLoader(val_data, batch_size=batch_size, shuffle=False, num_workers=2)
 testloader = DataLoader(test_data, batch_size=batch_size, shuffle=False, num_workers=2)
 
-
 # Build model
 model = Net()
-
 
 # Main training loop
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0.001)
@@ -128,79 +121,80 @@ val_loss_log = []
 
 for i in range(50):
 
-  # Run an epoch of training
-  train_running_loss = 0
-  train_running_acc = 0
-  model.train()
-  for j,input in enumerate(trainloader,0):
-   
-    x = input[0].to(device)
-    y = input[1].type(torch.LongTensor).to(device)
-    out = model(x)
-    loss = criterion(out,y)
+    # Run an epoch of training
+    train_running_loss = 0
+    train_running_acc = 0
+    model.train()
+    for j, input in enumerate(trainloader, 0):
+        x = input[0].to(device)
+        y = input[1].type(torch.LongTensor).to(device)
+        out = model(x)
+        loss = criterion(out, y)
 
-    model.zero_grad()
-    loss.backward()
+        model.zero_grad()
+        loss.backward()
 
-    optimizer.step()
+        optimizer.step()
 
-    _, predicted = torch.max(out.data, 1)
-    correct = (predicted == y).sum()
+        _, predicted = torch.max(out.data, 1)
+        correct = (predicted == y).sum()
 
-    train_running_loss += loss.item()
-    train_running_acc += correct.item()
-    loss_log.append(loss.item())
-    acc_log.append(correct.item()/len(y))
+        train_running_loss += loss.item()
+        train_running_acc += correct.item()
+        loss_log.append(loss.item())
+        acc_log.append(correct.item() / len(y))
 
-  train_running_loss /= j
-  train_running_acc /= len(train_data)
+    train_running_loss /= j
+    train_running_acc /= len(train_data)
 
-  # Evaluate on validation
-  val_acc = 0
-  val_loss = 0
-  model.eval()
-  for j,input in enumerate(valloader,0):
+    # Evaluate on validation
+    val_acc = 0
+    val_loss = 0
+    model.eval()
+    for j, input in enumerate(valloader, 0):
+        x = input[0].to(device)
+        y = input[1].type(torch.LongTensor).to(device)
 
-    x = input[0].to(device)
-    y = input[1].type(torch.LongTensor).to(device)
+        out = model(x)
 
-    
-    out = model(x)
+        loss = criterion(out, y)
+        _, predicted = torch.max(out.data, 1)
+        correct = (predicted == y).sum()
 
-    loss = criterion(out,y)
-    _, predicted = torch.max(out.data, 1)
-    correct = (predicted == y).sum()
+        val_acc += correct.item()
+        val_loss += loss.item()
 
-    val_acc += correct.item()
-    val_loss += loss.item()
+    val_acc /= len(val_data)
+    val_loss /= j
 
-  val_acc /= len(val_data)
-  val_loss /= j
+    val_acc_log.append(val_acc)
+    val_loss_log.append(val_loss)
 
-  val_acc_log.append(val_acc)
-  val_loss_log.append(val_loss)
-
-  logging.info("[Epoch {:3}]   Loss:  {:8.4}     Train Acc:  {:8.4}%      Val Acc:  {:8.4}%".format(i,train_running_loss, train_running_acc*100,val_acc*100))
-
+    logging.info(
+        "[Epoch {:3}]   Loss:  {:8.4}     Train Acc:  {:8.4}%      Val Acc:  {:8.4}%".format(i, train_running_loss,
+                                                                                             train_running_acc * 100,
+                                                                                             val_acc * 100))
 
 # Plot training and validation curves
-fig, ax1 = plt.subplots(figsize=(16,9))
+fig, ax1 = plt.subplots(figsize=(16, 9))
 color = 'tab:red'
 ax1.plot(range(len(loss_log)), loss_log, c=color, alpha=0.25, label="Train Loss")
-ax1.plot([np.ceil((i+1)*len(train_data)/batch_size) for i in range(len(val_loss_log))], val_loss_log,c="red", label="Val. Loss")
+ax1.plot([np.ceil((i + 1) * len(train_data) / batch_size) for i in range(len(val_loss_log))], val_loss_log, c="red",
+         label="Val. Loss")
 ax1.set_xlabel("Iterations")
 ax1.set_ylabel("Avg. Cross-Entropy Loss", c=color)
 ax1.tick_params(axis='y', labelcolor=color)
-ax1.set_ylim(-0.01,3)
+ax1.set_ylim(-0.01, 3)
 
 ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
 
 color = 'tab:blue'
 ax2.plot(range(len(acc_log)), acc_log, c=color, label="Train Acc.", alpha=0.25)
-ax2.plot([np.ceil((i+1)*len(train_data)/batch_size) for i in range(len(val_acc_log))], val_acc_log,c="blue", label="Val. Acc.")
+ax2.plot([np.ceil((i + 1) * len(train_data) / batch_size) for i in range(len(val_acc_log))], val_acc_log, c="blue",
+         label="Val. Acc.")
 ax2.set_ylabel(" Accuracy", c=color)
 ax2.tick_params(axis='y', labelcolor=color)
-ax2.set_ylim(-0.01,1.01)
+ax2.set_ylim(-0.01, 1.01)
 
 fig.tight_layout()  # otherwise the right y-label is slightly clipped
 ax1.legend(loc="center")
--- univariate_tester.py
+++ univariate_tester.py
@@ -1,8 +1,9 @@
 import numpy as np
 import itertools
 
+
 def s(x):
-  return 1/(1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
 # i gate
@@ -25,26 +26,25 @@ w_gx = 0
 w_gh = 0
 b_g = 0
 
-
 ################################
 
 # The below code runs through all length 14 binary strings and throws an error 
 # if the LSTM fails to predict the correct parity
 
 cnt = 0
-for X in itertools.product([0,1], repeat=14):
-  c=0
-  h=0
-  cnt += 1
-  for x in X:
-    i = s(w_ih*h + w_ix*x + b_i)
-    f = s(w_fh*h + w_fx*x + b_f)
-    g = np.tanh(w_gh*h + w_gx*x + b_g)
-    o = s(w_oh*h + w_ox*x + b_o)
-    c = f*c + i*g
-    h = o*np.tanh(c)
-  if np.sum(X)%2 != int(h>0.5):
-    print("Failure",cnt, X, int(h>0.5), np.sum(X)%2 == int(h>0.5))
-    break
-  if cnt % 1000 == 0:
-    print(cnt)
+for X in itertools.product([0, 1], repeat=14):
+    c = 0
+    h = 0
+    cnt += 1
+    for x in X:
+        i = s(w_ih * h + w_ix * x + b_i)
+        f = s(w_fh * h + w_fx * x + b_f)
+        g = np.tanh(w_gh * h + w_gx * x + b_g)
+        o = s(w_oh * h + w_ox * x + b_o)
+        c = f * c + i * g
+        h = o * np.tanh(c)
+    if np.sum(X) % 2 != int(h > 0.5):
+        print("Failure", cnt, X, int(h > 0.5), np.sum(X) % 2 == int(h > 0.5))
+        break
+    if cnt % 1000 == 0:
+        print(cnt)
