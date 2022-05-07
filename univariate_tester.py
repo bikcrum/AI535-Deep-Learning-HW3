32bcf33b004fc0c8921d788e8287a3e9252fbef0
bikram <bikcrum@gmail.com>
2022-05-07 02:25:23 -0700

    add manually parameter in lstm

--- univariate_tester.py
+++ univariate_tester.py
@@ -7,24 +7,24 @@ def s(x):
 
 
 # i gate
-w_ix = 0
-w_ih = 0
-b_i = 0
+w_ix = 100
+w_ih = 100
+b_i = -50
 
 # f gate
 w_fx = 0
 w_fh = 0
-b_f = 0
+b_f = -100
 
 # o gate
-w_ox = 0
-w_oh = 0
-b_o = 0
+w_ox = -100
+w_oh = -100
+b_o = 150
 
 # g
 w_gx = 0
 w_gh = 0
-b_g = 0
+b_g = 100
 
 ################################
 
@@ -37,11 +37,24 @@ for X in itertools.product([0, 1], repeat=14):
     h = 0
     cnt += 1
     for x in X:
+        # i = h or x
         i = s(w_ih * h + w_ix * x + b_i)
+
+        # f = 0
         f = s(w_fh * h + w_fx * x + b_f)
+
+        # g = 1
         g = np.tanh(w_gh * h + w_gx * x + b_g)
+
+        # o =  !(h and x)
         o = s(w_oh * h + w_ox * x + b_o)
+
+        # c = 0 * c + i * 1
+        # c = i
+        # c = h or x
         c = f * c + i * g
+
+        # h = !(h and x) * (h or x)
         h = o * np.tanh(c)
     if np.sum(X) % 2 != int(h > 0.5):
         print("Failure", cnt, X, int(h > 0.5), np.sum(X) % 2 == int(h > 0.5))
